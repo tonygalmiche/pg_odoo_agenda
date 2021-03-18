fdf5de0aa51352fda9646b8ad0d61b6b1b44288d
Tony Galmiche <tony.galmiche@infosaone.com>
2021-03-18 12:00:24 +0100

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "InfoSaône - Module Odoo 14 pour Agenda Plastigray",
+    "version" : "0.1",
+    "author" : "InfoSaône / Tony Galmiche",
+    "category" : "InfoSaône",
+    "description": """
+    InfoSaône - Module Odoo 14 pour Agenda Plastigray
+    ===================================================
+    InfoSaône - Module Odoo 14 pour Agenda Plastigray
+    """,
+    "maintainer": "InfoSaône",
+    "website": "https://infosaone.com",
+    "depends" : [
+        "base",
+        "calendar",
+    ], 
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data" : [
+        "security/ir.model.access.csv",
+        "security/ir.rule.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "active": False,
+    "application": True,
+}
