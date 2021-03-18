5e0a40154ed6c88eba836a21a382bf33d5a561ca
Tony Galmiche <tony.galmiche@infosaone.com>
2021-03-18 13:20:32 +0100

    bug

--- models/calendar.py
+++ models/calendar.py
@@ -2,22 +2,22 @@
 from odoo import api, fields, models, _
 
 
-# class Meeting(models.Model):
-#     _inherit = 'calendar.event'
+class Meeting(models.Model):
+    _inherit = 'calendar.event'
  
-#     @api.model_create_multi
-#     def create(self, vals_list):
-#         ## Bug création en double des repetitions sur la premiere journée ******
-#         if vals_list:
-#             v=vals_list[0]
-#             user_id=v.get("user_id")
-#             if user_id:
-#                 print('user_id=',user_id,vals_list)
-#                 start=v.get("start")
-#                 events = self.env['calendar.event'].search([('user_id', '=', user_id),('start', '=', start)], limit=1)
-#                 if events:
-#                     return self
-#         #***********************************************************************
+    @api.model_create_multi
+    def create(self, vals_list):
+        ## Bug création en double des repetitions sur la premiere journée ******
+        if vals_list:
+            v=vals_list[0]
+            user_id=v.get("user_id")
+            if user_id:
+                print('user_id=',user_id,vals_list)
+                start=v.get("start")
+                events = self.env['calendar.event'].search([('user_id', '=', user_id),('start', '=', start)], limit=1)
+                if events:
+                    return events
+        #***********************************************************************
 
-#         res=super().create(vals_list)
-#         return res
+        res=super().create(vals_list)
+        return res
