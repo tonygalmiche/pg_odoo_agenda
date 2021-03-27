59531f2c4a635cfec0fb0499ef64b7b32724ff6b
Tony Galmiche <tony.galmiche@infosaone.com>
2021-03-27 16:13:37 +0100

    modif

--- models/calendar.py
+++ models/calendar.py
@@ -2,14 +2,38 @@
 from odoo import api, fields, models, _
 
 
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+
+    def _ajouter_invitation_responsable_action(self):
+        for obj in self.browse(self.env.context['active_ids']):
+            for partner in obj.partner_ids:
+                if partner not in obj.attendee_ids.partner_id:
+                    print("Ajouter", partner.name,obj.id)
+                    vals={
+                        "event_id"  : obj.id,
+                        "partner_id": partner.id,
+                        "state"     : "accepted",
+                    }
+                    self.env['calendar.attendee'].create(vals)
+
+
+#   id   | event_id | partner_id |    state    |         common_name          | availability |           access_token           | create_uid |        create_date         | write_uid |         write_date         | is_user_id 
+# -------+----------+------------+-------------+------------------------------+--------------+----------------------------------+------------+----------------------------+-----------+----------------------------+------------
+#  10670 |     5015 |         65 | accepted    | Laetitia LEBRUN              |              | eab118c4b99344aa965bfc5d46ed7487 |         81 | 2021-03-22 10:14:20.049383 |        64 | 2021-03-22 10:17:38.760978 |         64
+#  10673 |     5016 |         65 | accepted    | Laetitia LEBRUN              |              | 2eca7e6a1ab44d368f81c7847c5eb016 |         81 | 2021-03-22 10:14:20.049383 |        64 | 2021-03-22 10:17:38.760978 |         64
+
+
+
+
+
 class CalendarAttendee(models.Model):
     _inherit = 'calendar.attendee'
-
 
     @api.depends('partner_id')
     def _compute_is_user_id(self):
         for obj in self:
-            print(obj,obj.partner_id)
             user_id=False
             if obj.partner_id:
                 users = self.env['res.users'].search([
@@ -17,7 +41,6 @@ class CalendarAttendee(models.Model):
                 ])
                 if users:
                     user_id=users[0].id
-                print(users,user_id)
             obj.is_user_id=user_id
 
 
@@ -37,8 +60,6 @@ class CalendarAttendee(models.Model):
 #                 test=True
 #             obj.is_je_participe=test
 #     is_je_participe = fields.Boolean('Je participe', compute='_compute_is_je_participe')
-
-
 
 
 # class Meeting(models.Model):
