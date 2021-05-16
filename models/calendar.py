38228e644e3f0dff1fb33e052efb38604780ff9e
Tony Galmiche <tony.galmiche@infosaone.com>
2021-05-16 15:38:05 +0200

    modif

--- models/calendar.py
+++ models/calendar.py
@@ -11,56 +11,31 @@ class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
 
 
+    def synchroniser_google_user(self,event,user):
+        with google_calendar_token(user.sudo()) as token:
+            if token:
+                _logger.warning("## token = %s" % (token))
+                headers = {'Content-type': 'application/json', 'Authorization': 'Bearer %s' % token}
+                params = {'access_token': token}
+                values = event._google_values()
+                _logger.warning("## values = %s" % (values))
+                google_service = GoogleCalendarService(self.with_user(user).env['google.service'])
+                event_id = values.get('id')
+                _logger.warning("## synchroniser_google_action event_id = %s" % (event_id))
+                try:
+                    _logger.warning("## _google_patch event = %s" % (event))
+                    event.with_user(user)._google_patch(google_service, event_id, values, timeout=3)
+                except:
+                    _logger.exception("## _google_patch  ERROR event = %s" % (event))
+
+
     def synchroniser_google_action(self):
-
-
-        _logger.warning("## synchroniser_google_action self = %s" % (self))
-
-
         for obj in self.browse(self.env.context['active_ids']):
-
-            #if not obj.google_id:
-            #    obj.google_id = uuid4().hex
-
-
-            _logger.warning("## synchroniser_google_action obj = %s" % (obj))
-
-            #if obj.google_id:
-            #event_id = obj.id
-            #_logger.warning("## Recherche si évènement existe avant de le créer event_id = %s" % (event_id))
-            #url = "/calendar/v3/calendars/primary/events/%s" % event_id
             for line in obj.attendee_ids:
                 user=line.is_user_id
+                _logger.warning("## synchroniser_google_action user = %s" % (user.login))
                 if user:
-                    with google_calendar_token(user.sudo()) as token:
-                        if token:
-                            _logger.warning("## token = %s" % (token))
-                            headers = {'Content-type': 'application/json', 'Authorization': 'Bearer %s' % token}
-                            params = {'access_token': token}
-                            values = obj._google_values()
-                            _logger.warning("## values = %s" % (values))
-                            google_service = GoogleCalendarService(self.with_user(user).env['google.service'])
-                            email  = line.email
-                            #values["organizer"]   = {'email': email, 'self': True}
-                            #values["attendees"]=[]
-                            #values["description"]="GoogleSync write\ntoto et tutu\nsur 2 lignes\n"+"\n"+email
-
-                            #if not values.get('id'):
-                            #    values['id'] = uuid4().hex
-
-                            event_id = values.get('id')
-                            _logger.warning("## synchroniser_google_action event_id = %s" % (event_id))
-
-
-                            #_logger.warning("## email,google_service = %s,%s" % (email, google_service))
-                            try:
-                                _logger.warning("## _google_patch email = %s" % (email))
-                                obj.with_user(user)._google_patch(google_service, event_id, values, timeout=3)
-                            except:
-                                _logger.exception("## _google_patch  ERROR email = %s" % (email))
-
-
-
+                    self.synchroniser_google_user(obj,user)
 
 
     @api.onchange('partner_ids','start','duration')
@@ -119,11 +94,6 @@ class CalendarEvent(models.Model):
             obj.is_participants = "<br />".join(lines)
 
 
-
-
-
-
-
     is_alerte       = fields.Text('Alerte'      , copy=False, compute=_compute_is_alerte)
     is_participants = fields.Text('Participants', copy=False, compute=_compute_is_participants)
 
@@ -151,7 +121,6 @@ class CalendarEvent(models.Model):
     #             'view_id': self.env.ref('pg_odoo_agenda.view_calendar_event_calendar_journee').id,
     #             'domain': [["start","<=","2021-07-04 21:59:59"],["stop",">=","2021-06-27 22:00:00"],["partner_ids","in",[3,7]]],
     #         }
-    #         print(res)
     #         return res
 
 
@@ -169,8 +138,6 @@ class CalendarAttendee(models.Model):
                 if users:
                     user_id=users[0].id
             obj.is_user_id=user_id
-
-
     is_user_id = fields.Many2one('res.users', 'Utilisateur', compute='_compute_is_user_id', store=True, readonly=True, index=True)
 
 
@@ -178,46 +145,15 @@ class CalendarAttendee(models.Model):
     def _accepter_invitation_actions(self):
         for obj in self.browse(self.env.context['active_ids']):
             obj.state="accepted"
-            obj.event_id.sudo().synchroniser_google_action()
-            print(obj)
+
 
     def _refuser_invitation_actions(self):
         for obj in self.browse(self.env.context['active_ids']):
             obj.state="declined"
-            obj.event_id.sudo().synchroniser_google_action()
-            print(obj)
 
 
+    def write(self, vals):
+        res = super(CalendarAttendee, self).write(vals)
+        self.env['calendar.event'].synchroniser_google_user(self.event_id,self.is_user_id)
+        return res
 
-# class Meeting(models.Model):
-#     _inherit = 'calendar.event'
-
-
-#     def _compute_is_je_participe(self):
-#         for obj in self:
-#             test=False
-#             if self.env.user.partner_id in obj.partner_ids:
-#                 test=True
-#             obj.is_je_participe=test
-#     is_je_participe = fields.Boolean('Je participe', compute='_compute_is_je_participe')
-
-
-# class Meeting(models.Model):
-#     _inherit = 'calendar.event'
- 
-#     @api.model_create_multi
-#     def create(self, vals_list):
-#         ## Bug création en double des repetitions sur la premiere journée ******
-#         if vals_list:
-#             v=vals_list[0]
-#             user_id=v.get("user_id")
-#             if user_id:
-#                 print('user_id=',user_id,vals_list)
-#                 start=v.get("start")
-#                 events = self.env['calendar.event'].search([('user_id', '=', user_id),('start', '=', start)], limit=1)
-#                 if events:
-#                     return events
-#         #***********************************************************************
-
-#         res=super().create(vals_list)
-#         return res
